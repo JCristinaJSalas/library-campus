0d0355526410dd6d9e3419f10340dc9d164d53a1
JCristinaJSalas <89360648+JCristinaJSalas@users.noreply.github.com>
2023-10-04 23:16:40 -0500

    feat: :sparkles: Create the validations for the objects
    Create Number,String,Date,Object validations

--- storge/validations.js
+++ storge/validations.js
@@ -1,31 +1,35 @@
 export const isString = (dato) => {
-  const mensaje = {
+  const mensajeString = {
     status: 400,
     message: `El dato no es un string, es un ${dato.constructor.name}`,
   };
-  typeof dato === "string" ? console.log("Es un string") : console.log(mensaje);
+  const resString =
+    typeof dato === "string" ? true : console.log(mensajeString);
+  return resString;
 };
 
 export const isNumber = (dato) => {
-  const mensaje = {
+  const mensajeNumber = {
     status: 400,
-    message: `El dato no es un numero, es un ${dato.constructor.name}`,
+    message: `El dato no es un numero, es un ${typeof dato}`,
   };
-  typeof dato === "number" ? console.log("es numero") : console.log(mensaje);
+  const resNumber =
+    typeof dato === "number" ? true : console.log(mensajeNumber);
+  return resNumber;
 };
 
 export const isDate = (dato) => {
-  const mensaje = {
+  const mensajeDate = {
     status: 400,
     message: `El dato no es una fecha, es un ${typeof dato}`,
   };
-  dato.constructor.name === "Date"
-    ? console.log("Es una fecha")
-    : console.log(mensaje);
+  const resDate =
+    dato.constructor.name === "Date" ? true : console.log(mensajeDate);
+  return resDate;
 };
 
 export const isObject = (dato) => {
-  const mensaje = {
+  const mensajeObject = {
     status: 400,
     message: `El dato no es una objeto, es un ${typeof dato}`,
   };
@@ -33,9 +37,69 @@ export const isObject = (dato) => {
 
   const resultado =
     typeof dato !== "object"
-      ? mensaje
+      ? console.log(mensajeObject)
       : Object.keys(dato).length === 0
-      ? mensajeVacio
-      : dato;
+      ? console.log(mensajeVacio)
+      : true;
   return resultado;
+};
+
+export const objectBooks = (dataObject) => {
+  const objectNew = {};
+  const keyBook = [
+    "title",
+    "date",
+    "authorId",
+    "categoryId",
+    "editionId",
+    "isbn",
+    "numPages",
+    "id",
+  ];
+  Object.entries(dataObject).forEach((data) => {
+    data[0] === "title"
+        ? isString(data[1])
+          ? (objectNew[data[0]] = data[1])
+          :  false
+        : "";
+    data[0] === "date"
+        ? isDate(new Date(data[1]))
+          ? (objectNew[data[0]] = data[1])
+          :  false
+        : "";
+    data[0] === "authorId"
+        ? isNumber(data[1])
+          ? (objectNew[data[0]] = data[1])
+          :  false
+        : "";
+    data[0] === "categoryId"
+        ? isNumber(data[1])
+          ? (objectNew[data[0]] = data[1])
+          :  false
+        : "";
+    data[0] === "editionId"
+        ? isNumber(data[1])
+          ? (objectNew[data[0]] = data[1])
+          :  false
+        : "";
+    data[0] === "isbn"
+        ? isNumber(data[1])
+          ? (objectNew[data[0]] = data[1])
+          :  false
+        : "";
+    data[0] === "numPages"
+        ? isNumber(data[1])
+          ? (objectNew[data[0]] = data[1])
+          :  false
+        : "";
+    data[0] === "id"
+        ? isNumber(data[1])
+          ? (objectNew[data[0]] = data[1])
+          :  false
+        : "";
+  });
+  const keys = Object.keys(objectNew);
+  const mensajeNoCompleto = { status: 400, message: `El objeto no esta completo` };
+  const respuesta = keyBook.every((key) => keys.includes(key)) ? objectNew : mensajeNoCompleto;
+  return respuesta;
 };
