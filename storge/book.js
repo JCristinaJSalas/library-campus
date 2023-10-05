c8caa42a2513b875a847b22f37b42d0f406c99c2
JCristinaJSalas <89360648+JCristinaJSalas@users.noreply.github.com>
2023-10-04 23:15:47 -0500

    feat: :sparkles: Create function(getAll and getOne) on Books

--- storge/book.js
+++ storge/book.js
@@ -1,18 +1,20 @@
-import { isString,isNumber,isDate, isObject } from "./validations.js"
+import { isObject, objectBooks } from "./validations.js";
 
+export const getAllBooks = async (urlBooks) => {
+  const dataBooksJson = await (await fetch(urlBooks)).json();
+  const dataBooks = dataBooksJson.forEach((book) => {
+    if (!isObject(book)) return false; //validamos si no esta vacio y es objeto
+    const res = objectBooks(book); // Validamos la estructura
+    return res;
+  });
+  return dataBooks;
+};
 
-export const getBook = (dataObject) => {
-  console.log(isObject(dataObject)); 
-  const keyBook = ["title","date","authorId","categoryId","editionId","isbn","numPaginations","id"]
-
-  Object.keys(dataObject).forEach((e) => {
-    if (keyBook.includes(e)){
-       console.log(`Si incluye ${e} `)
-    }
-    else{
-      const mensaje = "El dato no esta en el objeto"
-      return console.log(mensaje)
-    }
-  })
-}
-
+export const getOneBook = async (urlBooks, id) => {
+  const urlId = urlBooks + "/" + id;
+  const bookJson = await (await fetch(urlId)).json();
+  if (!isObject(bookJson)) return false; //validamos si no esta vacio y es objeto
+  const res = objectBooks(bookJson); // Validamos la estructura
+  //console.log(res)
+  return res;
+};
